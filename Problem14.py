760a892cb88f0a424603fac9058d0a2c7b1e9c1f
Du Lin <lnidux@gmail.com>
2016-01-22 18:43:49 +0100

    Risolto 14 in Python

--- Problem14.py
+++ Problem14.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python
 
-print "Hello world"
+# Longest Collatz Sequence
+
+nd = {}
+
+def sequence(n):
+    num = 0
+    n_start = n
+    while n != 1:
+        if str(n) in nd:
+            num += nd[str(n)]
+            break
+        elif n % 2 == 0:
+            n = n/2
+        else:
+            n = 3*n + 1
+        num += 1
+
+    nd[str(n_start)] = num
+    return num
+
+max = 0
+for n in xrange(1, 1000000):
+    num = sequence(n)
+    if num > max:
+        max = num
+        max_n_start = n
+
+print(max_n_start)
